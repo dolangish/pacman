e97874fee49c575af33c741f0f7c4267dc51d40e
Dolan Gish <maddawgx9@comcast.net>
2014-01-23 20:17:55 -0800

    moved game into its own class, a red rectangle now moves left to right across the screen

--- pacman_main.py
+++ pacman_main.py
@@ -1,14 +1,7 @@
 import pygame, sys
 from pygame.locals import *
 
-pygame.init()
-
 FPS = 30
-fpsClock = pygame.time.Clock()
-
-border_rect=pygame.Rect(50,50,600,600)
-DISPLAYSURF = pygame.display.set_mode((700, 700))
-pygame.display.set_caption("Pacman")
 
 BROWN = (205,133,63)
 GRAY = (155,155,155)
@@ -19,15 +12,65 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 YELLOW = (255,255,0)
 
-while True: # main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
+class Entity():
+    
+    def __init__(self, color):
+        self.color=color
+        self.rect=pygame.Rect(100,100,200,200)
+
+    def update(self):
+        if self.rect.x < 300:
+            self.rect.x=self.rect.x+1        
+
+    def draw(self, surface):
+        pygame.draw.rect(surface,self.color,self.rect)
+
+class Game():
+
+    def __init__(self, surface):
+        self.surface=surface
+        self.entity_list=[]
+
+    def initialize(self):
+        self.clock = pygame.time.Clock()
+        self.border_rect=pygame.Rect(50,50,600,600)
+        pygame.display.set_caption("Pacman")
         
-    DISPLAYSURF.fill(BG_COL)
-    pygame.draw.rect(DISPLAYSURF,BLUE,border_rect,3);
+        self.entity_list.append(Entity(RED))
+        return True;
 
-    pygame.display.update()
-    fpsClock.tick(FPS)
+    def update(self):
+        for entity in self.entity_list:
+            entity.update()
 
+    def draw(self):
+        self.surface.fill(BG_COL)
+        pygame.draw.rect(self.surface,BLUE,self.border_rect,3)
+
+        for entity in self.entity_list:
+            entity.draw(self.surface)
+
+        pygame.display.update()
+
+    def run(self):
+        while True: # main game loop
+            for event in pygame.event.get():
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                    pygame.quit()
+                    sys.exit()
+                    
+            self.update()
+                    
+            self.draw()                    
+            
+            self.clock.tick(FPS)
+
+def main():
+   pygame.init()
+   surface = pygame.display.set_mode((700, 700))
+   game=Game(surface);
+   if game and game.initialize():
+       game.run();
+
+if __name__=="__main__":
+    main()
